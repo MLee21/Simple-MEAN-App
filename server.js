0952296e53526e048bef6959988cd6c9ce0737ca
MLee21 <minnie.lee89@gmail.com>
2016-01-07 15:23:24 -0700

    generated Express routes.

--- server.js
+++ server.js
@@ -20,3 +20,43 @@ console.log('App listening on port 8080');
 var Todo = mongoose.model('Todo', {
   text : String
 });
+
+// routes
+
+app.get('/api/todos', function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err)
+      res.send(err)
+    res.json(todos);
+  });
+});
+
+app.post('/api/todos', function(req, res) {
+  Todo.create({
+    text : req.body.text,
+    done : false 
+  }, function(err, todo){
+    if (err)
+      res.send(err);
+
+    Todo.find(function(err, todos){
+      if (err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
+});
+
+app.delete('/api/todos/:todo_id', function(req, res){
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo){
+    if (err)
+      res.send(err);
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
+});
