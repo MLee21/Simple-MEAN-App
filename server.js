207b083bbfdc16c3b44c87f740926b5109b3fdba
MLee21 <minnie.lee89@gmail.com>
2016-01-07 15:06:39 -0700

    configured app.

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uq03mypu');
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+app.listen(8080);
+console.log('App listening on port 8080');
